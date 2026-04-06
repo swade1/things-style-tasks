841bbedb24c4249e2de4c0301df034c12e07faf7
Susan Wade <susan.wade09@gmail.com>
2026-04-06 15:25:59 -0600

    Updating README to remove extraneous content

--- src/App.tsx
+++ src/App.tsx
@@ -88,24 +88,3 @@ function App() {
 
 export default App
 
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
-
-export default App
